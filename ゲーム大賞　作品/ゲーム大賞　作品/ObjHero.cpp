edd7d02164271c7f55f4f7bd7d1288326e9f4745
綾斗 冨永 <h1202019023@hiratagakuen.onmicrosoft.com>
2021-03-19 12:19:45 +0900

    主人公移動

--- ゲーム大賞　作品/ゲーム大賞　作品/ObjHero.cpp
+++ ゲーム大賞　作品/ゲーム大賞　作品/ObjHero.cpp
@@ -1,6 +1,6 @@
 //使用するヘッダーファイル
 #include"GameL\DrawTexture.h"
-#include"GameL\DrawTexture.h"
+#include"GameL\WinInputs.h"
 #include"GameL\SceneManager.h"
 
 #include"GameHead.h"
@@ -12,12 +12,36 @@ using namespace GameL;
 //イニシャライズ
 void CObjHero::Init()
 {
-
+	 m_px=0.0f;//位置
+	 m_py=0.0f;
+	 m_vx=0.0f;//移動ベクトル
+	 m_vy=0.0f;
+	 m_posture = 1.0f;//右向き0.0f左向き1.0f
 }
 
 //アクション
 void CObjHero::Action()
 {
+	//移動ベクトルの破棄
+	m_vx=0.0f;
+	m_vy=0.0f;
+
+	//キーの入力方向
+	if (Input::GetVKey(VK_RIGHT) == true)
+	{
+		m_vx = +1.0f;
+		m_posture = 1.0f;
+	}
+
+	if (Input::GetVKey(VK_LEFT) == true)
+	{
+		m_vx = -1.0f;
+		m_posture = 0.0f;
+	}
+
+	//位置の更新
+	m_px += m_vx;
+	m_py += m_vy;
 
 }
 
@@ -36,10 +60,11 @@ void CObjHero::Draw()
 	src.m_right = 656.0f;
 	src.m_bottom = 800.0f;
 
-	dst.m_top = 0.0f;
-	dst.m_left = 0.0f;
-	dst.m_right = 64.0f;
-	dst.m_bottom = 64.0f;
+	//表示位置の設定
+	dst.m_top = 0.0f+m_py;
+	dst.m_left =( 64.0f*m_posture)+m_px;
+	dst.m_right = (64.0-64.0f*m_posture)+m_px;
+	dst.m_bottom = 64.0f+m_py;
 
 	//描画
 	Draw::Draw(0, &src, &dst, c, 0.0f);
--- ゲーム大賞　作品/ゲーム大賞　作品/ObjHero.h
+++ ゲーム大賞　作品/ゲーム大賞　作品/ObjHero.h
@@ -15,4 +15,9 @@ public:
 	void Action();//アクション
 	void Draw();//ドロー
 private:
+	float m_px;//位置
+	float m_py;
+	float m_vx;//移動ベクトル
+	float m_vy;
+	float m_posture;//姿勢
 };
