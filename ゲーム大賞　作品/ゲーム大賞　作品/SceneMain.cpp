01ac7ed5a86bb428a0cd70169e18902a20df39ce
綾斗 冨永 <h1202019023@hiratagakuen.onmicrosoft.com>
2021-03-19 11:20:33 +0900

    SceneMain

---
+++ ゲーム大賞　作品/ゲーム大賞　作品/SceneMain.cpp
@@ -0,0 +1,39 @@
+//STLデバッグ機能をOFFにする
+#define _SECURE_SCL (0)
+#define _HAS_ITERATOR_DEBUGGING (0)
+
+//GameLで使用するヘッダー
+#include "GameL\SceneObjManager.h"
+#include"GameL\DrawTexture.h"
+
+
+//使用するネームスペース
+using namespace GameL;
+
+//使用ヘッダー
+#include"SceneMain.h"
+#include"GameHead.h"
+
+//コンストラクタ
+CSceneMain::CSceneMain()
+{
+
+}
+
+//デストラクタ
+CSceneMain::~CSceneMain()
+{
+
+}
+
+//初期化メソッド
+void CSceneMain::InitScene()
+{
+
+}
+
+//実行中メソッド
+void CSceneMain::Scene()
+{
+
+}
