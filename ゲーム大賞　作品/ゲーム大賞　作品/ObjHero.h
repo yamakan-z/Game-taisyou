b52e62169f34cfe378f47b5f784cf9f9a0dd97c6
綾斗 冨永 <h1202019023@hiratagakuen.onmicrosoft.com>
2021-03-19 11:59:10 +0900

    主人公表示

--- ゲーム大賞　作品/ゲーム大賞　作品/ObjHero.h
+++ ゲーム大賞　作品/ゲーム大賞　作品/ObjHero.h
@@ -1,1 +1,18 @@
 #pragma once
+//使用するヘッダーファイル
+#include"GameL\SceneObjManager.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//シーン：メイン
+class CObjHero :public CObj
+{
+public:
+	CObjHero() {};
+	~CObjHero() {};
+	void Init();//イニシャライズ
+	void Action();//アクション
+	void Draw();//ドロー
+private:
+};
--- ゲーム大賞　作品/ゲーム大賞　作品/SceneMain.cpp
+++ ゲーム大賞　作品/ゲーム大賞　作品/SceneMain.cpp
@@ -29,7 +29,12 @@ CSceneMain::~CSceneMain()
 //初期化メソッド
 void CSceneMain::InitScene()
 {
+	//グラフィック読み込み
+	Draw::LoadImageW(L"仮主人公.png", 0, TEX_SIZE_512);
 
+	//主人公オブジェクト作成
+	CObjHero* obj = new CObjHero();
+	Objs::InsertObj(obj, OBJ_HERO, 10);
 }
 
 //実行中メソッド
