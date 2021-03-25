4a50013715845a311a4eb720f63304d91063070c
颯人 永原 <h1202016002@hiratagakuen.onmicrosoft.com>
2021-03-25 15:12:49 +0900

    上下移動の挙動調整中

--- ゲーム大賞　作品/ゲーム大賞　作品/ObjHero.h
+++ ゲーム大賞　作品/ゲーム大賞　作品/ObjHero.h
@@ -39,6 +39,11 @@ private:
 	float m_vy;
 	float m_posture;//姿勢
 
+	
+	bool up_key_flag;//はしごのキーフラグ
+	bool right_flag;//右のキーフラグ
+	bool left_flag;//左のキーフラグ
+
 	//blockとの衝突状態確認用
 	bool m_hit_up;
 	bool m_hit_down;
