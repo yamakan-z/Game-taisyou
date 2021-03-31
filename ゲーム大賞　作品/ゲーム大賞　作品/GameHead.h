42bd6158605d61f4aa8501c1b62c21bf803ec3a6
颯人 永原 <h1202016002@hiratagakuen.onmicrosoft.com>
2021-03-31 16:30:46 +0900

    タイトル修正

--- ゲーム大賞　作品/ゲーム大賞　作品/GameHead.h
+++ ゲーム大賞　作品/ゲーム大賞　作品/GameHead.h
@@ -111,5 +111,5 @@ struct UserData
 
 //シーンスタートクラス---------------------------
 //ゲーム開始時のシーンクラス登録
-#define SET_GAME_START  CSceneMain
+#define SET_GAME_START  CSceneTitle
 //-----------------------------------------------
