d57dfd7bcb33696adfcb0bbc4b5afc5dd3300ce0
栞大 山本 <h1202016006@hiratagakuen.onmicrosoft.com>
2021-04-23 12:34:54 +0900

    はしご表示（個別で）作成

--- ゲーム大賞　作品/ゲーム大賞　作品/ObjGameOver.cpp
+++ ゲーム大賞　作品/ゲーム大賞　作品/ObjGameOver.cpp
@@ -32,7 +32,7 @@ void CObjGameOver::Init()
 	((UserData*)Save::GetData())->ins_flag = false;
 	((UserData*)Save::GetData())->break_flag = false;
 	((UserData*)Save::GetData())->ins_ladder = false;
-	
+	((UserData*)Save::GetData())->ladder = false;
 
 	//インベントリ関係のフラグ破棄
 	((UserData*)Save::GetData())->I_board1 = false;
