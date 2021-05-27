851af31ef195bd47bc9e276b12e1648371899710
綾斗 冨永 <h1202019023@hiratagakuen.onmicrosoft.com>
2021-05-27 10:31:43 +0900

    タイトル

--- ゲーム大賞　作品/ゲーム大賞　作品/ObjTitle.cpp
+++ ゲーム大賞　作品/ゲーム大賞　作品/ObjTitle.cpp
@@ -56,9 +56,9 @@ void CObjTitle::Action()
 //ドロー
 void CObjTitle::Draw()
 {
-	float c[4] = { 1.0f,-1.0f,-1.0f,1.0f };
-	//float r[4] = { -1.0f,1.0f,-1.0f,4.0f };
-	Font::StrDraw(L"考え中", 325, 220, 56, c);
-
-	Font::StrDraw(L"GameStart:EnterKey",220, 400, 44, c);
+	//float c[4] = { 1.0f,-1.0f,-1.0f,1.0f };
+	////float r[4] = { -1.0f,1.0f,-1.0f,4.0f };
+	//Font::StrDraw(L"考え中", 325, 220, 56, c);
+	//
+	//Font::StrDraw(L"GameStart:EnterKey",220, 400, 44, c);
 }
