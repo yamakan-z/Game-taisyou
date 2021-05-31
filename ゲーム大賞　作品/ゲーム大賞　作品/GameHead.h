557316ab10bff7e5d4698463737615bc3d897328
本山佑亮 <h1202015003@hiratagakuen.onmicrosoft.com>
2021-05-31 15:07:46 +0900

    ステージ2,3編集

--- ゲーム大賞　作品/ゲーム大賞　作品/GameHead.h
+++ ゲーム大賞　作品/ゲーム大賞　作品/GameHead.h
@@ -184,5 +184,5 @@ struct UserData
 
 //シーンスタートクラス---------------------------
 //ゲーム開始時のシーンクラス登録
-#define SET_GAME_START  CSceneTitle
+#define SET_GAME_START  CSceneMain3
 //-----------------------------------------------
